23cb508399966ea994236e46f09e34447faaaaff
omarbara <omarbara@ymail.com>
2019-11-25 09:17:10 +0100

    dd

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,20 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+
+  //convert to uppercase for better matching
+  string = string.toUpperCase()
+
+  //chek for repeted charachters and assigne them to 0
+  var tmpStr = string.split('')
+  for (var i = 0; i < tmpStr.length; i++) {
+  	for (var j = i+1; j < tmpStr.length; j++) {
+  		if(tmpStr[i] === tmpStr[j]){
+  			tmpStr.splice(i,1,0)
+  			tmpStr.splice(j,1,0)
+  		}
+  	}
+  }
+ //print firtst charachter which is not assigne to 0 
+  return tmpStr.find(ele => ele !== 0)  
 };
