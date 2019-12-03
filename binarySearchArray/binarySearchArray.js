4d6fbddf02a39f459a06e8eea18f3906115ae369
omarbara <omarbara@ymail.com>
2019-12-03 09:50:11 +0100

    _data

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,38 @@
  */
 
 var binarySearch = function (array, target) {
+
+
+	//do mid
+	var midIndx = array.length / 2
+	midIndx = parseInt(midIndx)
+	if(array[midIndx] === target){
+		return midIndx
+	}
+
+	// compare if bigger go right else go left
+	if(target > array[midIndx] && array.length >= 0){
+		// go right
+		var rightSide = array.slice(midIndx)
+
+		//do mid
+		binarySearch(rightSide,target)
+		// var midIndx = rightSide.length / 2
+		// midIndx = parseInt(midIndx)
+	}
+
+	// go left
+	else {
+		var leftSide = array
+		leftSide.length = midIndx 
+		//do mid
+		midIndx = parseInt(leftSide.length / 2)
+		if(leftSide[midIndx] == target){
+			return midIndx * 2
+		}
+		//
+
+	}
+
 };
 
