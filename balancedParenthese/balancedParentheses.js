fbc50f9af9de597c46c033d2ffb9d0f5bb0129fe
omarbara <omarbara@ymail.com>
2019-12-20 09:33:10 +0100

    balancedParanthesis

--- balancedParenthese/balancedParentheses.js
+++ balancedParenthese/balancedParentheses.js
@@ -8,4 +8,27 @@ isBalanced("(50)(")	// false
 isBalanced("") //	true
 */
 
-var isBalanced = function(str) {};
+var isBalanced = function(str) {
+var open =0 , close =0;
+var arr = str.split('')
+
+if(str.length === 0){
+	return true
+}
+
+for (var i = 0; i < arr.length; i++) {
+	console.log(arr[i])
+	if(arr[i] === '('){
+		open++;
+	}
+	if(arr[i] === ')'){
+		close++
+	}
+}
+
+if(open === close){
+	return true
+}
+return false
+
+};
