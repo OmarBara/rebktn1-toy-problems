89de44f53759b66c8b0d2c3b7156b3269a65e033
omarbara <omarbara@ymail.com>
2019-12-26 09:53:17 +0100

    Anagrams

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -14,4 +14,21 @@
 
 var allAnagrams = function(string) {
   // Your code here.
+
+var arr = string.split('')
+
+for (var i = 0; i < arr.length; i++) {
+	arr[i].splice(i,1,arr[i+1])
+	console.log(arr[i])
+}
+
 };
+
+/*
+ 
+  for (var i = 0; i < string.length; i++) {
+    var fChar = string[i];
+    var chars =string.substring(0, i)+string.substring(i + 1);     
+  }
+  
+*/
