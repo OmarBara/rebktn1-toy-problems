5867e631558d3b15c727a95f338c7770313558c3
omarbara <omarbara@ymail.com>
2019-12-28 09:54:15 +0100

    evenOcurence

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -7,3 +7,22 @@ evenOccurrence([ "cat", "dog", "dig", "cat" ]) // "cat"
 evenOccurrence([[1,2,3], [1,2], [1,2,3]]) // [1,2,3]
 evenOccurrence([{a: 1, b: 2}, {a:1, b:3}, {a:1, b:2}]) // {a:1, b:2}
 */
+
+var evenOccurrence = function(array){
+
+var result ;
+var count = 0; 
+// 
+for (var i = 0; i < array.length; i++) { 
+    for (var j = i+1; j <array.length; j++){ 
+        if (array[i] === array[j]){ 
+            count++; 
+        }
+    } 
+        if (count === 2) 
+          	 return array[i]; 
+} 
+
+
+// return counts
+}
