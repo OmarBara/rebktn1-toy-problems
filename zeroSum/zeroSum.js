41700208a3f2400d0347a3f031151c38f2a9ac90
omarbara <omarbara@ymail.com>
2019-12-31 09:26:06 +0100

    zeroSum

--- zeroSum/zeroSum.js
+++ zeroSum/zeroSum.js
@@ -7,3 +7,14 @@ Constraints: Solve this linear time complexity O(n)
 zeroSum([ 1, 3, 2, -3 ]) // true
 zeroSum([ 5, 7, 2, 9 ]) // false
 */
+function zeroSum(nums) {
+var array =[]
+	for (var i = 0; i < nums.length; i++) {
+		if(nums.filter(function(number) {
+  		return number+nums[i] === 0;
+		}).length >0){
+			return true
+		}
+	}
+	return false
+}
