18ea392006ea9cc870d2636f5dcd9084ea9a04a0
Omar Bara <omarbara@ymail.com>
2020-01-14 10:05:24 +0100

    array

--- ArrayCeption/arrayCeption.js
+++ ArrayCeption/arrayCeption.js
@@ -15,3 +15,31 @@
 //  [ ] ==>	0
 //  array:
 //  [ [ [ ] ] ] ==>	0
+
+var Arrayception = function(array) {
+	// body...
+	let count = 0;
+	//chck the typeof array is array 
+	 // if === undefined the loop is finish
+	 //if not add 1 to count
+	// while(typeof(argument) !== 'undefined'){
+	// 	argument = argument[0]
+	// 	console.log()
+
+	// }
+	function loop(array){
+	for (let i=0; i<array.length; i++){
+		//type of array is array and have value
+      if (Array.isArray(array[i]) && array[i].length > 0 )
+        // console.log()
+    count++
+    loop(array)
+	 //if the brevious count typeof == number return count
+	 //else return 0
+	}
+}
+
+loop(array)
+
+return count;
+}
